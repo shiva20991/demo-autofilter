28557dfa7b0c38971288b73aa53a51403337d09b
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2021-05-12 18:42:04 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,12 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>WELCOME TO SUNNY_TALK.OUR OFFICIAL CHANNEL @TAMILROCKERSVPN @TRVPN
 
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+CREATED BY TRVPN ADMIN X-MEN and SOUL 
+CREDITS TO :RIDER SOUL 300PER MONTH
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ DEVELOPED</b> : <b><i><a href="https://t.me/trvpn">TRVPN</a></i></b>
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➥ Language</b> : <code>Tamil</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ DATABASE</b> : <i><a href="https://t.me/trvpn">@tamilrockersvpn </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Channel</b> : <i><a href="https://github.com/t.me/trvpn">Click Me</a></i>
 """
